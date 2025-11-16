a8990e74bfee68282b239fcbfaa04b57812b43ab
sourabh17gupta <mkktlguptas@gmail.com>
2025-11-16 15:22:25 +0530

    Update TeamMemberSchema.js

--- models/TeamMemberSchema.js
+++ models/TeamMemberSchema.js
@@ -5,39 +5,46 @@ const TeamMemberSchema = new mongoose.Schema(
     name: { 
       type: String, 
       required: true,
+      trim: true,
       index: true,
     },
     email: {
       type: String, 
       required: true,
       unique: true, // unique email
+      trim: true,
       index: true,
     },
     InstagramId: {
       type: String,
-      default: null, // optional, duplicates allowed
+      default: null, // duplicates allowed
+      trim: true,
     },
     LinkdinId: {
       type: String,
-      default: null, // optional, duplicates allowed
+      default: null, // duplicates allowed
+      trim: true,
     },
     Position: {
       type: String, 
       required: true,
+      trim: true,
       index: true,
     },
     TeamName: {
       type: String,
       enum: ["managment", "design", "contentwriting", "core"],
       required: true,
+      trim: true,
       index: true,
     },
     Image: {
       type: String,
     },
-    Year:{
+    Year: {
       type: String,
       required: true,
+      trim: true,
     }
   },
   { timestamps: true }
