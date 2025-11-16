57898086bbbb9cddb8ddca753d35c8fa8606d0e0
sourabh17gupta <mkktlguptas@gmail.com>
2025-11-16 13:44:03 +0530

    Update cloudinary.js

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,14 +1,13 @@
 const cloudinary = require("cloudinary").v2;
 
 exports.cloudinaryConnect = () => {
-  try {
-    cloudinary.config({
-      cloud_name: process.env.CLOUD_NAME,
-      api_key: process.env.API_KEY,
-      api_secret: process.env.API_SECRET
-    });
-    console.log("Cloudinary connected successfully");
-  } catch (error) {
-    console.error("Cloudinary connection error:", error);
-  }
+	try {
+		cloudinary.config({
+			cloud_name: process.env.CLOUD_NAME,
+			api_key: process.env.API_KEY,
+			api_secret: process.env.API_SECRET,
+		});
+	} catch (error) {
+		console.log(error);
+	}
 };
