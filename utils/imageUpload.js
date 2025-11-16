b992a30b28c34a92bfa9b862d1942f828feb8110
sourabh17gupta <mkktlguptas@gmail.com>
2025-11-16 13:04:42 +0530

    Update imageUpload.js

--- utils/imageUpload.js
+++ utils/imageUpload.js
@@ -1,7 +1,9 @@
-const cloudinary = require("cloudinary").v2;
-
-async function uploadImage(file, Folder){
-    return await cloudinary.uploader.upload(file.tempFilePath, {resource_type:"auto", folder: Folder});
+async function uploadImage(file, Folder) {
+  const result = await cloudinary.uploader.upload(file.tempFilePath, {
+    resource_type: "auto",
+    folder: Folder,
+  });
+  return { url: result.secure_url }; // <-- always HTTPS
 }
 
 module.exports = uploadImage;
