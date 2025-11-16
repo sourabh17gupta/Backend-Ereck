b3ff9ca9e3d00a7d55a623d25e1c1e72f554dbfa
sourabh17gupta <mkktlguptas@gmail.com>
2025-11-16 14:10:50 +0530

    Update teamController.js

--- controllers/teamController.js
+++ controllers/teamController.js
@@ -41,22 +41,12 @@ exports.getTeamDetails = async (req, res) => {
       });
     }
 
-    // Indexed lookup on TeamDetail.teamName
-    const teamDetails = await TeamDetail.findOne({ teamName }).lean();
-    if (!teamDetails) {
-      return res.status(404).json({
-        success: false,
-        message: "Team not found",
-      });
-    }
-
     // Indexed lookup on TeamMember.TeamName
     const teamMembers = await TeamMember.find({ TeamName: teamName }).lean();
 
     res.status(200).json({
       success: true,
       data: {
-        description: teamDetails.description,
         teamMembers,
       },
     });
